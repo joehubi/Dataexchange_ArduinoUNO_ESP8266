e7226392b79111280212f4b84092356ada31a209
joehubi <joehubi@gmail.com>
2024-09-17 12:13:00 +0200

    Arduino: error corrected at dataexchange read 	- sscanf was deleted in prior version ESP8266: changed using dataexchange.h

--- DX_ESP8266/DX_ESP8266.ino
+++ DX_ESP8266/DX_ESP8266.ino
@@ -1,93 +1,96 @@
-#include <SoftwareSerial.h>
+// ################### Johannes Huber 
+// ################### 17.09.2024
 
-// ################### Global
-  #define PRINT_VARIABLE(var) Serial.print(#var " = "); Serial.println(var);
+// ################### DEFINITION
+  // ################### Lib
+    #include <dataexchange.h>
+  // ################### macros
+    #define PRINT_VARIABLE(var) Serial.print(#var " = "); Serial.println(var);
+  // ################### variables
+    // ################### dataexchange
+      #define rxPin D5
+      #define txPin D6
+      dataexchange DATAX_ARDU_ESP(rxPin, txPin);
 
-// ################### SoftwareSerial
-  #define rxPin D5
-  #define txPin D6
-  SoftwareSerial SoftwareSerial_ESP8266(rxPin, txPin);
-  // ################### READ
-    char incoming_char_array[50]; // max. number of signs in data string
-    int read_ctn = 0;         
-    int dummy = 0;            // type "byte" does not work
-  // ################### SEND
-    char send_char_array[50];
-    int snd_ctn = 5;          
+      // ################### READ
+        char received_data[50]; // max. number of signs in data string
+        int read_ctn = 0;         
+        int dummy = 0;            // type "byte" does not work
+      // ################### SEND
+        char data_to_send[50];
+        int snd_ctn = 5;          
 
-// ############################ Timer for timed loops
-  const bool debug_timers = false;
+    // ################### Timer for timed loops
+      struct timer {
+        int cycle_ms            = 1000;      
+        unsigned long cycle_dt  = 0;    
+        const bool debug        = false;  
+      };
 
-  const int cycle_50ms          = 50;
-  unsigned long cycle_50ms_dt   = 0;
+      timer t1000ms;    // build instance of timer
+      timer t1500ms;
+      timer t50ms;
 
-  const int cycle_1000ms          = 1000;      
-  unsigned long cycle_1000ms_dt   = 0;
+      unsigned long millisec;           // arduino time-ms
 
-  const int cycle_1500ms          = 1500;
-  unsigned long cycle_1500ms_dt   = 0;
+// ################### SETUP
+  void setup() {
+    // ################### Debug Monitor
+      Serial.begin(9600);
+      while (!Serial) {
+        ; // wait for serial port to connect
+      }
 
-  unsigned long millisec;           // arduino time-ms
+    // ################### SoftwareSerial
+      pinMode(rxPin, INPUT);
+      pinMode(txPin, OUTPUT);
+      DATAX_ARDU_ESP.begin(9600);
+    // ################### timers
+      t1000ms.cycle_ms = 1000;  
+      t1500ms.cycle_ms = 1500;  
+      t50ms.cycle_ms = 50;    
+  }
+// ################### LOOP
+  void loop() {
 
-void setup() {
-  // ################### Debug Monitor
-    Serial.begin(9600);
-    while (!Serial) {
-      ; // wait for serial port to connect
+    millisec = millis();      // get time from arduino-clock (time since arduino is running in ms)
+
+  // ################### 50 ms
+    if (millisec - t50ms.cycle_dt >= t50ms.cycle_ms) {
+        t50ms.cycle_dt = millisec;
+
+      // ################### dataexchange READ
+      if (DATAX_ARDU_ESP.rxData(received_data, 50)) {  // check if data is available and get data
+        PRINT_VARIABLE(received_data);  // debugging
+      }
+
+      sscanf(received_data, "%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u", &dummy, &dummy, &dummy, &dummy, &dummy, &dummy, &dummy, &dummy, &dummy, &dummy, &dummy, &dummy, &read_ctn);
+      PRINT_VARIABLE(read_ctn);
+  
     }
 
-  // ################### SoftwareSerial
-    pinMode(rxPin, INPUT);
-    pinMode(txPin, OUTPUT);
-    SoftwareSerial_ESP8266.begin(9600);
+  // ################### 1000 ms
+    if (millisec - t1000ms.cycle_dt >= t1000ms.cycle_ms) {
+      // ################### 1000 ms Timer
+        t1000ms.cycle_dt = millisec;
+        if (t1000ms.debug == true) {
+          Serial.println("Start 1000 ms");
+        }
+    }
 
-  Serial.println("SETUP - software serial ESP8266");
-}
+  // ################### 1500 ms
+    if (millisec - t1500ms.cycle_dt >= t1500ms.cycle_ms) {
+      // ################### 1500 ms Timer
+        t1500ms.cycle_dt = millisec;
+        if (t1500ms.debug == true) {
+          Serial.println("Start 1500 ms");
+        }
 
-void loop() {
+      // ################### dataexchange SEND
+        snd_ctn++;
+        sprintf(data_to_send, "%u,111,222,333,444,555,666,777,888,999,1000", snd_ctn);
+        DATAX_ARDU_ESP.txData(data_to_send);
+        PRINT_VARIABLE(data_to_send); // debugging
 
-  millisec = millis();      // get time from arduino-clock (time since arduino is running in ms)
-
-// ################### 50 ms
-  if (millisec - cycle_50ms_dt >= cycle_50ms) {
-    // 50 ms Timer
-      cycle_50ms_dt = millisec;
-    // ################### READ
-      if (SoftwareSerial_ESP8266.available()) {
-        delay(50);  // short delay that all received data is present at the Rx
-        String _incoming_string = SoftwareSerial_ESP8266.readStringUntil('\n'); // read Rx until \n
-        _incoming_string.toCharArray(incoming_char_array, 50); // copy data to char-Array (for sscanf)
-        PRINT_VARIABLE(incoming_char_array);  //debugging
-
-        // decode data from char-array
-        sscanf(incoming_char_array, "%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u", &dummy, &dummy, &dummy, &dummy, &dummy, &dummy, &dummy, &dummy, &dummy, &dummy, &dummy, &dummy, &read_ctn);
-        PRINT_VARIABLE(read_ctn);
-      }    
+    }
   }
-
-// ################### 1000 ms
-  if (millisec - cycle_1000ms_dt >= cycle_1000ms) {
-    // ################### 1000 ms Timer
-      cycle_1000ms_dt = millisec;
-      if (debug_timers == true) {
-        Serial.println("Start 1000 ms");
-      }
-  }
-
-// ################### 1500 ms
-  if (millisec - cycle_1500ms_dt >= cycle_1500ms) {
-    // ################### 1500 ms Timer
-      cycle_1500ms_dt = millisec;
-      if (debug_timers == true) {
-        Serial.println("Start 1500 ms");
-      }
-
-    snd_ctn++;
-
-    // ################### SEND
-      sprintf(send_char_array, "%u,111,222,333,444,555,666,777,888,999,1000", snd_ctn);
-      SoftwareSerial_ESP8266.println(send_char_array);
-      PRINT_VARIABLE(send_char_array);
-
-  }
-}
